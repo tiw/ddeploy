3b9e2a02826e5463412353ac2f82fea99933f464
Ting Wang <tting.wang@gmail.com>
2014-08-20 09:38:30 +0800

    make watch interval configable.

--- commands/monitor.py
+++ commands/monitor.py
@@ -20,7 +20,9 @@ class Monitor(DockerBase):
             ids = ",".join(stopped_container_id)
             text = "Container %s are down!" % ids
             self.send_email(text, email_list)
-        threading.Timer(10, self.monitor_containers, [container_ids, email_list]).start()
+        threading.Timer(
+            self.config.get('base', 'WATCH_INTERVAL'), self.monitor_containers, [container_ids, email_list]
+        ).start()
 
     def monitor_container(self, container_id, email_list):
         pprint(container_id)
@@ -28,7 +30,9 @@ class Monitor(DockerBase):
         if not status['State']['Running']:
             text = "Container %s is down!" % container_id
             self.send_email(text, email_list)
-        threading.Timer(10, self.monitor_container, [container_id, email_list]).start()
+        threading.Timer(
+            self.config.get('base', 'WATCH_INTERVAL'), self.monitor_container, [container_id, email_list]
+        ).start()
 
     def monitor(self, group_name):
         pass
