f9b545a263c1d06acce13fcf8ea74ea3a18c6fdb
Ting Wang <tting.wang@gmail.com>
2014-08-26 09:46:29 +0800

    Still using manager to set host.

--- rest_server.py
+++ rest_server.py
@@ -3,13 +3,16 @@ from pprint import pprint
 
 __author__ = 'wangting'
 from flask import Flask, jsonify, Response
+from flask_script import Manager, Shell
 from utils import DockerUtils, parser_config
 
 app = Flask(__name__)
+app.debug = True
 import os.path
 _dir = os.path.dirname(os.path.abspath(__file__))
 config = parser_config(os.path.join(_dir, 'config.json'))
 
+manager = Manager(app=app)
 
 @app.route('/containers')
 def list_containers():
@@ -28,4 +31,7 @@ def start_container(container_id):
         return Response(status=500)
     return Response(status=200)
 
-app.run()
+
+
+
+manager.run()
